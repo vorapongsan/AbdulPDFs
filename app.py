ca0144f7b9051cfed2bb0a2fc60ede27132e2305
toche <t.samanchuen.gmail.com>
2024-10-05 03:52:09 +0700

    update lib to community

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores  import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI  
+from langchain_community.chat_models import ChatOpenAI  
 from htmlTemplates import css, bot_template, user_template
 
 
